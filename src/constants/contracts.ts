589b55f32b02649a2a58b40fed6f36b706a48ad1
Nanaland <huy23121994@gmail.com>
2024-08-15 15:19:15 +0700

    update contract

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -10,8 +10,8 @@ export const TOKENS: {
     { symbol: "USDT", address: "0x01eF7ED545FC2632c4c69de706441e0327dFE0Dc" },
   ],
   [FHENIX_CHAIN_ID_LOCAL]: [
-    { symbol: "USDT", address: "0xade09a9b557B6DFb410D8e68f396991dADD8354A" },
-    { symbol: "DAI", address: "0x2D9E715712Be986F97aF613a7aa5424BA4F6e2b4" },
+    { symbol: "USDT", address: "0x20E068a567630483901Af229a6eCF5eb401A5D66" },
+    { symbol: "DAI", address: "0xf621eb91625Eb049d7E3c03497Faf8bfE9B72823" },
   ],
 };
 
@@ -22,9 +22,9 @@ export const TOKEN_LOGO: { [name: string]: string } = {
 
 export const POOL: { [chainId: number]: `0x${string}` } = {
   [FHENIX_CHAIN_ID]: "0x58c210109bFa288D0Aca084599bE04c6f43591B2",
-  [FHENIX_CHAIN_ID_LOCAL]: "0x58c210109bFa288D0Aca084599bE04c6f43591B2",
+  [FHENIX_CHAIN_ID_LOCAL]: "0x5B0ABc8c4e9Cd491C2A204bf8581Da3Ad9284ff9",
 };
 export const POOL_CORE: { [chainId: number]: `0x${string}` } = {
   [FHENIX_CHAIN_ID]: "0x48e310B5dcecb5D17C3aC59624Ac61468DE7d108",
-  [FHENIX_CHAIN_ID_LOCAL]: "0x48e310B5dcecb5D17C3aC59624Ac61468DE7d108",
+  [FHENIX_CHAIN_ID_LOCAL]: "0xc172fc2df2E4841DFf0e2A8395318E51dB031053",
 };
