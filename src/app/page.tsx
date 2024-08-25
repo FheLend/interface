2d59f8930a8815c80dd52be22762c755cd9eb931
Nanaland <huy23121994@gmail.com>
2024-08-25 23:55:58 +0700

    responsive for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,12 +44,8 @@ function GradientText(props: TextProps) {
 
 function Home() {
   return (
-    <Box
-      bg={`url(${background.src})`}
-      bgSize={{ base: "cover", lg: "cover" }}
-      minH="100vh"
-    >
-      <Flex px="10" py="5" alignItems="center">
+    <Box bg={`url(${background.src})`} bgSize="cover" minH="100vh">
+      <Flex px={{ base: 5, md: 10 }} py="5" alignItems="center">
         <Box as={Link} href="/" mr="5">
           <Image src={logo} alt="logo" />
         </Box>
@@ -73,7 +69,7 @@ function Home() {
             </Center>
           );
         })}
-        <Center ml="auto">
+        <Center ml="auto" display={{ base: "none", md: "flex" }}>
           <Button as={Link} href="/dashboard" variant="outline">
             Launch app
           </Button>
@@ -81,11 +77,14 @@ function Home() {
       </Flex>
 
       <Container maxW="container.md" minH="calc(100vh - 80px)" display="flex">
-        <Center flexDir="column" textAlign="center">
-          <GradientText fontSize="xxx-large" fontWeight="bold">
+        <Center flexDir="column" textAlign={{ md: "center" }}>
+          <GradientText
+            fontSize={{ base: "xx-large", md: "xxx-large" }}
+            fontWeight={{ base: "semibold", md: "bold" }}
+          >
             Empowering Confidential and Secure Crypto Lending
           </GradientText>
-          <Text color="whiteAlpha.700" px="10" my="7" lineHeight="30px">
+          <Text color="whiteAlpha.700" px={{ md: 10 }} my="7" lineHeight="30px">
             Felend is a pioneering lending protocol powered by Fully Homomorphic
             Encryption (FHE). We provide a secure way to lend and borrow crypto
             assets while ensuring that your financial details remain completely
@@ -93,16 +92,25 @@ function Home() {
             sensitive information is protected while you benefit from the full
             potential of DeFi
           </Text>
-          <Button as={Link} href="/dashboard" w="200px">
+          <Button
+            as={Link}
+            href="/dashboard"
+            w="200px"
+            alignSelf={{ base: "flex-start", md: "center" }}
+          >
             Launch App
           </Button>
         </Center>
       </Container>
 
       <Container maxW="container.lg" mt="20">
-        <Flex>
+        <Flex flexDir={{ base: "column", md: "row" }}>
           <Image src={earthIcon} alt="felend-icon" />
-          <Box px="20" ml="10">
+          <Box
+            px={{ base: 0, md: 20 }}
+            mt={{ base: 5, md: 0 }}
+            ml={{ base: 0, md: 10 }}
+          >
             <GradientText>Security and Confidentiality First</GradientText>
             <Text
               color="whiteAlpha.700"
@@ -121,9 +129,13 @@ function Home() {
             </Text>
           </Box>
         </Flex>
-        <Flex mt="20">
+        <Flex mt="20" flexDir={{ base: "column", md: "row" }}>
           <Image src={peopleIcon} alt="felend-icon" />
-          <Box px="20" ml="10">
+          <Box
+            px={{ base: 0, md: 20 }}
+            mt={{ base: 5, md: 0 }}
+            ml={{ base: 0, md: 10 }}
+          >
             <GradientText>Fair Lending</GradientText>
             <Text
               color="whiteAlpha.700"
@@ -140,9 +152,13 @@ function Home() {
             </Text>
           </Box>
         </Flex>
-        <Flex mt="20">
+        <Flex mt="20" flexDir={{ base: "column", md: "row" }}>
           <Image src={networkIcon} alt="felend-icon" />
-          <Box px="20" ml="10">
+          <Box
+            px={{ base: 0, md: 20 }}
+            mt={{ base: 5, md: 0 }}
+            ml={{ base: 0, md: 10 }}
+          >
             <GradientText>Cross-Chain Lending (Upcoming)</GradientText>
             <Text
               color="whiteAlpha.700"
@@ -162,8 +178,8 @@ function Home() {
         </Flex>
       </Container>
 
-      <Container maxW="container.lg" mt="300px" p="0">
-        <SimpleGrid columns={2} gap={20}>
+      <Container maxW="container.xl" mt={{ base: "100px", md: "300px" }}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} gap={20}>
           <Box>
             <Image src={bankerIcon} alt="felend-icon" />
             <Box mt="10">
@@ -200,11 +216,11 @@ function Home() {
         </SimpleGrid>
       </Container>
 
-      <Box mt="300px" textAlign="center">
+      <Box mt={{ base: "100px", md: "300px" }} textAlign="center">
         <GradientText fontSize="xx-large" fontWeight="bold">
           Join with us
         </GradientText>
-        <Center mt="20" gridGap={20}>
+        <Center mt={{ base: 10, md: 20 }} gridGap={20}>
           <Image src={discordIcon} alt="felend-icon" />
           <Image src={telegramIcon} alt="felend-icon" />
           <Image src={xIcon} alt="felend-icon" />
