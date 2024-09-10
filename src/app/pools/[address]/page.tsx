ecd6170f09e7795734dc472c907bccf30bf6db7d
Bao Huy Bao Huy <huy23121994@gmail.com>
2024-09-10 20:39:50 +0700

    update params

--- src/app/pools/[address]/page.tsx
+++ src/app/pools/[address]/page.tsx
@@ -12,6 +12,7 @@ import {
   Grid,
   GridItem,
   Image,
+  Text,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { useChainId, useReadContract, useReadContracts } from "wagmi";
@@ -22,6 +23,11 @@ import RepayForm from "@/app/components/repay/repayForm";
 import WithdrawForm from "@/app/components/withdrawModal/withdrawForm";
 import { get } from "lodash";
 import poolAbi from "@/constants/abi/pool.json";
+import tokenAbi from "@/constants/abi/token.json";
+import { formatUnits } from "viem";
+
+const RAY = 10 ** 27; // 10 to the power 27
+const SECONDS_PER_YEAR = 31536000;
 
 function VerticalDivider() {
   return (
@@ -66,23 +72,60 @@ function PoolDetail({ params }: { params: { address: string } }) {
   // address aTokenAddress,
   // uint40 lastUpdateTimestamp
 
-  const { data, refetch, isLoading } = useReadContract({
-    address: POOL[chainId],
-    abi: poolAbi,
-    functionName: "getReserveData",
-    args: [poolAddress],
+  const { data, refetch, isLoading } = useReadContracts({
+    contracts: [
+      {
+        address: POOL[chainId],
+        abi: poolAbi,
+        functionName: "getReserveData",
+        args: [poolAddress],
+      },
+      {
+        address: POOL[chainId],
+        abi: poolAbi,
+        functionName: "getReserveConfigurationData",
+        args: [poolAddress],
+      },
+      {
+        address: poolAddress as `0x${string}`,
+        abi: tokenAbi,
+        functionName: "decimals",
+      },
+    ],
   });
 
-  const totalLiquidity = get(data, "[0]", 0n);
-  const availableLiquidity = get(data, "[1]", 0n);
-  const totalBorrowsStable = get(data, "[2]", 0n);
-  const totalBorrowsVariable = get(data, "[3]", 0n);
-  const liquidityRate = get(data, "[4]", 0n);
-  const variableBorrowRate = get(data, "[5]", 0n);
-  const stableBorrowRate = get(data, "[6]", 0n);
-  const averageStableBorrowRate = get(data, "[7]", 0n);
-  const utilizationRate = get(data, "[8]", 0n);
-  const aTokenAddress = get(data, "[11]", "");
+  const reserveData = get(data, "[0].result", []) as any[];
+  const reserveConfigurationData = get(data, "[1].result", []) as any[];
+
+  const tokenDecimals = get(data, "[2].result", 18) as number;
+  const totalLiquidity = get(reserveData, "[0]", 0n);
+  const availableLiquidity = get(reserveData, "[1]", 0n);
+  const totalBorrowsStable = get(reserveData, "[2]", 0n);
+  const totalBorrowsVariable = get(reserveData, "[3]", 0n);
+  const liquidityRate = get(reserveData, "[4]", 0n);
+  const variableBorrowRate = get(reserveData, "[5]", 0n);
+  const stableBorrowRate = get(reserveData, "[6]", 0n);
+  const averageStableBorrowRate = get(reserveData, "[7]", 0n);
+  const utilizationRate = get(reserveData, "[8]", 0n);
+  const aTokenAddress = get(reserveData, "[11]", "");
+
+  const ltv = get(reserveConfigurationData, "[0]", 0n);
+  const liquidationThreshold = get(reserveConfigurationData, "[1]", 0n);
+  const liquidationBonus = get(reserveConfigurationData, "[2]", 0n);
+  const usageAsCollateralEnabled = get(reserveConfigurationData, "[4]", false);
+  const borrowingEnabled = get(reserveConfigurationData, "[5]", false);
+  const stableBorrowRateEnabled = get(reserveConfigurationData, "[6]", false);
+  console.log(reserveConfigurationData, ltv);
+  const depositAPR = Number(liquidityRate) / RAY;
+  const depositAPY =
+    ((1 + depositAPR / SECONDS_PER_YEAR) ^ SECONDS_PER_YEAR) - 1;
+
+  const stableBorrowAPR = Number(variableBorrowRate) / RAY;
+  const variableBorrowAPR = Number(variableBorrowRate) / RAY;
+  const variableBorrowAPY =
+    ((1 + variableBorrowAPR / SECONDS_PER_YEAR) ^ SECONDS_PER_YEAR) - 1;
+  const stableBorrowAPY =
+    ((1 + stableBorrowAPR / SECONDS_PER_YEAR) ^ SECONDS_PER_YEAR) - 1;
 
   if (!token) {
     return <NotFound />;
@@ -103,16 +146,35 @@ function PoolDetail({ params }: { params: { address: string } }) {
       <Flex my="6" align="center">
         <Box>
           <Box color="whiteBlue.700" mb="3">
-            Supply
+            Total Liquidity
           </Box>
-          <Box fontSize="2xl">$116.61M</Box>
+          <Box fontSize="2xl">
+            {(+formatUnits(totalLiquidity, tokenDecimals)).toLocaleString()}
+            <Text as="span" ml="1" color="whiteBlue.700">
+              {token.symbol}
+            </Text>
+          </Box>
         </Box>
         <VerticalDivider />
         <Box>
           <Box color="whiteBlue.700" mb="3">
-            Liquidity
+            Available Liquidity
           </Box>
-          <Box fontSize="2xl">$116.61M</Box>
+          <Box fontSize="2xl">
+            {(+formatUnits(availableLiquidity, tokenDecimals)).toLocaleString()}
+            <Text as="span" ml="1" color="whiteBlue.700">
+              {token.symbol}
+            </Text>
+          </Box>
+        </Box>
+        <VerticalDivider />
+        <Box>
+          <Box color="whiteBlue.700" mb="3">
+            Utilization Rate
+          </Box>
+          <Box fontSize="2xl">
+            {(+formatUnits(utilizationRate, tokenDecimals)).toLocaleString()} %
+          </Box>
         </Box>
       </Flex>
 
@@ -124,14 +186,29 @@ function PoolDetail({ params }: { params: { address: string } }) {
                 <Box color="whiteBlue.700" fontSize="sm" mb="3">
                   Total Supply
                 </Box>
-                <Box fontSize="lg">$116.61M</Box>
+                <Box fontSize="lg">
+                  {(+formatUnits(
+                    totalLiquidity,
+                    tokenDecimals
+                  )).toLocaleString()}
+                  <Text as="span" ml="1" color="whiteBlue.700">
+                    {token.symbol}
+                  </Text>
+                </Box>
               </Box>
               <VerticalDivider />
               <Box>
                 <Box color="whiteBlue.700" fontSize="sm" mb="3">
-                  Total Supply
+                  APR
                 </Box>
-                <Box fontSize="lg">$116.61M</Box>
+                <Box fontSize="lg">{(depositAPR * 100).toLocaleString()}%</Box>
+              </Box>
+              <VerticalDivider />
+              <Box>
+                <Box color="whiteBlue.700" fontSize="sm" mb="3">
+                  APY
+                </Box>
+                <Box fontSize="lg">{(depositAPY * 100).toLocaleString()}%</Box>
               </Box>
             </Flex>
           </Card>
@@ -139,37 +216,98 @@ function PoolDetail({ params }: { params: { address: string } }) {
             <Flex mt="6" align="center">
               <Box>
                 <Box color="whiteBlue.700" fontSize="sm" mb="3">
-                  Total Supply
+                  Total Borrowed
                 </Box>
-                <Box fontSize="lg">$116.61M</Box>
+                <Box fontSize="lg">
+                  {(+formatUnits(
+                    totalLiquidity - availableLiquidity,
+                    tokenDecimals
+                  )).toLocaleString()}
+                  <Text as="span" ml="1" color="whiteBlue.700">
+                    {token.symbol}
+                  </Text>
+                </Box>
               </Box>
               <VerticalDivider />
               <Box>
                 <Box color="whiteBlue.700" fontSize="sm" mb="3">
-                  Total Supply
+                  APR, variable
                 </Box>
-                <Box fontSize="lg">$116.61M</Box>
+                <Box fontSize="lg">
+                  {(variableBorrowAPR * 100).toLocaleString()}%
+                </Box>
+              </Box>
+              <VerticalDivider />
+              <Box>
+                <Box color="whiteBlue.700" fontSize="sm" mb="3">
+                  APY, variable
+                </Box>
+                <Box fontSize="lg">
+                  {(variableBorrowAPY * 100).toLocaleString()}%
+                </Box>
+              </Box>
+              <VerticalDivider />
+              <Box>
+                <Box color="whiteBlue.700" fontSize="sm" mb="3">
+                  APR, stable
+                </Box>
+                <Box fontSize="lg">
+                  {(stableBorrowAPR * 100).toLocaleString()}%
+                </Box>
+              </Box>
+              <VerticalDivider />
+              <Box>
+                <Box color="whiteBlue.700" fontSize="sm" mb="3">
+                  APY, stable
+                </Box>
+                <Box fontSize="lg">
+                  {(stableBorrowAPY * 100).toLocaleString()}%
+                </Box>
               </Box>
             </Flex>
           </Card>
-          <Card title="Market info" mt="6" px="8">
+          <Card title="Pool info" mt="6" px="8">
             <RowInfo>
               <Box color="whiteBlue.700" fontSize="sm">
-                Total Supply
+                a{token.symbol} address
               </Box>
-              <Box>$116.61M</Box>
+              <Box>{aTokenAddress}</Box>
             </RowInfo>
             <RowInfo>
               <Box color="whiteBlue.700" fontSize="sm">
-                Total Supply
+                Loan to value
               </Box>
-              <Box>$116.61M</Box>
+              {Number(ltv).toLocaleString()} %
             </RowInfo>
             <RowInfo>
               <Box color="whiteBlue.700" fontSize="sm">
-                Total Supply
+                Liquidation threshold
               </Box>
-              <Box>$116.61M</Box>
+              {Number(liquidationThreshold).toLocaleString()} %
+            </RowInfo>
+            <RowInfo>
+              <Box color="whiteBlue.700" fontSize="sm">
+                Liquidation Bonus
+              </Box>
+              {Number(liquidationBonus).toLocaleString()} %
+            </RowInfo>
+            <RowInfo>
+              <Box color="whiteBlue.700" fontSize="sm">
+                Usage as collateral
+              </Box>
+              {`${usageAsCollateralEnabled}`}
+            </RowInfo>
+            <RowInfo>
+              <Box color="whiteBlue.700" fontSize="sm">
+                Borrowing
+              </Box>
+              {`${borrowingEnabled}`}
+            </RowInfo>
+            <RowInfo>
+              <Box color="whiteBlue.700" fontSize="sm">
+                Stable borrow rate
+              </Box>
+              {`${stableBorrowRateEnabled}`}
             </RowInfo>
           </Card>
         </GridItem>
